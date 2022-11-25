1ff3d6b99d892e53b99942033b32e9533f6ca52c
mAguilarSolvd <maguilar@solvd.com>
2022-11-25 14:35:13 -0300

    Minor changes

--- src/test/java/org/mja123/BaseTest.java
+++ src/test/java/org/mja123/BaseTest.java
@@ -1,15 +1,11 @@
 package org.mja123;
 
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.ios.IOSDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import java.io.FileInputStream;
